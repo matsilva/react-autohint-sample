88e7b196ce50e5fbeaa407de4a31d3875d2095f8
Mat Silva <askmatsilva@gmail.com>
2015-05-31 12:01:47 -0400

    step-2: Extract search input and add onchange handler

--- components/App.js
+++ components/App.js
@@ -6,14 +6,16 @@ var React = require('react');
 
 var AutohintApp = React.createClass({
 
+    sendSearchValue: function(val) {
+        console.log(val);
+    },
+
     render: function() {
         return (
             <div className="row">
                 <div className="col s4 offset-s4">
                     <div className="row">
-                        <div className="input-field col s12">
-                            <input name="search" placeholder="enter a city?" ref="search" type="text" />
-                        </div>
+                        <SearchInput sendSearchValue={this.sendSearchValue} />
                     </div>
                 </div>
             </div>
@@ -21,6 +23,29 @@ var AutohintApp = React.createClass({
     }
 
 });
+
+var SearchInput = React.createClass({
+
+    propTypes: {
+        sendSearchValue: React.PropTypes.func.isRequired,
+    },
+
+    getSearchValue: function() {
+        var searchInput = React.findDOMNode(this.refs.search);
+        var searchVal = searchInput.value;
+        this.props.sendSearchValue(searchVal);
+    },
+
+    render: function() {
+        return (
+            <div className="input-field col s12">
+                <input onChange={this.getSearchValue} name="search" placeholder="enter a city?" ref="search" type="text" />
+            </div>
+        );
+    }
+
+});
+
 
 
 React.render(<AutohintApp/>, document.getElementById('autohint'));
